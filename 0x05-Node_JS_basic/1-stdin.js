a3057a1927d607692faa0278605e597133ed9729
DAVE <jesugbohunmi23@gmail.com>
2025-01-20 18:56:53 +0100

    Update welcome message and improve name output in 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
 const process = require('process');
 
 // Display welcome message
-process.stdout.write('Welcome to ALX, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 // Handle stdin input
 process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
+    process.stdout.write(`Your name is: ${data}`);
 });
 
 // Handle program exit
