1ed8760d9b74e5fe33ef8b606a96455fd0c9c8b7
DAVE <jesugbohunmi23@gmail.com>
2025-01-20 22:40:33 +0100

    Update greeting message in HTTP server response

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -37,7 +37,7 @@ const app = http.createServer(async (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   
   if (req.url === '/') {
-    res.end('Hello ALX!');
+    res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     try {
       const data = await countStudents(process.argv[2]);
