4dbdd7253462a7bf2d91cd80ea4562a797a36970
DAVE <jesugbohunmi23@gmail.com>
2025-01-21 07:05:21 +0100

    Refactor readDatabase function for improved readability and conciseness; ensure consistent export formatting in routes and server files

--- 0x05-Node_JS_basic/full_server/utils/utils.js
+++ 0x05-Node_JS_basic/full_server/utils/utils.js
@@ -1,28 +1,26 @@
 import fs from 'fs';
 
-const readDatabase = (filePath) => {
-  return new Promise((resolve, reject) => {
-    fs.promises.readFile(filePath, 'utf8')
-      .then((data) => {
-        const lines = data.trim().split('\n');
-        const students = {};
-        
-        // Skip header row and process student data
-        for (let i = 1; i < lines.length; i += 1) {
-          const line = lines[i];
-          if (line) {
-            const [firstName, , , field] = line.split(',');
-            if (!students[field]) {
-              students[field] = [];
-            }
-            students[field].push(firstName);
+const readDatabase = (filePath) => new Promise((resolve, reject) => {
+  fs.promises.readFile(filePath, 'utf8')
+    .then((data) => {
+      const lines = data.trim().split('\n');
+      const students = {};
+
+      // Skip header row and process student data
+      for (let i = 1; i < lines.length; i += 1) {
+        const line = lines[i];
+        if (line) {
+          const [firstName, , , field] = line.split(',');
+          if (!students[field]) {
+            students[field] = [];
           }
+          students[field].push(firstName);
         }
-        
-        resolve(students);
-      })
-      .catch(() => reject(new Error('Cannot load the database')));
-  });
-};
+      }
+
+      resolve(students);
+    })
+    .catch(() => reject(new Error('Cannot load the database')));
+});
 
 export default readDatabase;
