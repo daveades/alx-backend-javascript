0d7b986f94d7c64009b0495c6f26ca83e03c8552
DAVE <jesugbohunmi23@gmail.com>
2025-01-21 06:08:04 +0100

    Refactor countStudents function to use Promise chaining and update greeting message in HTTP server response

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -5,42 +5,43 @@ const app = express();
 const port = 1245;
 
 function countStudents(path) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const data = await fs.readFile(path, 'utf8');
-      const lines = data.trim().split('\n');
-      const students = lines.slice(1).filter((line) => line);
-      let output = `Number of students: ${students.length}\n`;
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8')
+      .then((data) => {
+        const lines = data.trim().split('\n');
+        const students = lines.slice(1).filter((line) => line);
+        let output = `Number of students: ${students.length}\n`;
 
-      const fields = {};
-      students.forEach((student) => {
-        const [firstname, , , field] = student.split(',');
-        if (!fields[field]) fields[field] = [];
-        fields[field].push(firstname);
+        const fields = {};
+        students.forEach((student) => {
+          const [firstname, , , field] = student.split(',');
+          if (!fields[field]) fields[field] = [];
+          fields[field].push(firstname);
+        });
+
+        for (const [field, names] of Object.entries(fields)) {
+          output += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
+        }
+
+        resolve(output.trim());
+      })
+      .catch(() => {
+        reject(new Error('Cannot load the database'));
       });
-
-      for (const [field, names] of Object.entries(fields)) {
-        output += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
-      }
-
-      resolve(output);
-    } catch (error) {
-      reject(new Error('Cannot load the database'));
-    }
   });
 }
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+  res.send('Hello ALX!');
 });
 
 app.get('/students', async (req, res) => {
+  const message = 'This is the list of our students\n';
   try {
-    const message = 'This is the list of our students\n';
     const data = await countStudents(process.argv[2]);
-    res.send(message + data);
+    res.send(`${message}${data}`);
   } catch (error) {
-    res.send(message + error.message);
+    res.send(`${message}${error.message}`);
   }
 });
 
