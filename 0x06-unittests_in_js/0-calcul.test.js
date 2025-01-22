55ec00c9cc9472ffa289fd84d5fb51437366c46e
DAVE <jesugbohunmi23@gmail.com>
2025-01-22 18:45:33 +0100

    Add calculateNumber function and corresponding tests

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
+
+describe('calculateNumber', function() {
+    it('should return 4 when inputs are 1 and 3', function() {
+        assert.strictEqual(calculateNumber(1, 3), 4);
+    });
+
+    it('should return 5 when inputs are 1 and 3.7', function() {
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+    });
+
+    it('should return 5 when inputs are 1.2 and 3.7', function() {
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    });
+
+    it('should return 6 when inputs are 1.5 and 3.7', function() {
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    });
+
+    it('should return 0 when inputs are 0.1 and 0.3', function() {
+        assert.strictEqual(calculateNumber(0.1, 0.3), 0);
+    });
+
+    it('should return -2 when inputs are -1.4 and -0.5', function() {
+        assert.strictEqual(calculateNumber(-1.4, -0.5), -2);
+    });
+});
